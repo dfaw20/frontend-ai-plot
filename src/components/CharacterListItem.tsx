955e7e4112773a6c6e30d5743e0255a0107525ee
dfaw20 <dfaw200@gmail.com>
2023-09-09 12:30:16 +0900

    feature/enhance-character-list-display

--- src/components/CharacterListItem.tsx
+++ src/components/CharacterListItem.tsx
@@ -14,13 +14,21 @@ function CharacterListItem(props: CharacterListItemProps) {
 			<div className='flex justify-between'>
 				<div>
 					<div className='font-bold text-black-800'>{props.character.Name}</div>
-					<div className='text-gray-500'>{props.character.Nickname}</div>
+					<div className='flex items-center justify-center'>
+						<div className='text-gray-500'>
+							{props.character.Nickname}
+						</div>
+						<div className='ml-2 bg-slate-600 text-slate-50 px-2 text-sm flex items-center justify-center h-full'>
+							{getGenderText(props.character.Gender)}
+						</div>
+					</div>
 				</div>
 				<div className="flex items-center justify-center">
 					<RoundedButton/>
 				</div>
 			</div>
-			<div>{getGenderText(props.character.Gender)}</div>
+
+			
 			<div>{props.character.Outfit}</div>
 			<div>{props.character.Hairstyle}</div>
 			<div>{props.character.Personality}</div>
