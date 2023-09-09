945ddccf04d99ef53f3f307519a82b268e9e5f8d
dfaw20 <dfaw200@gmail.com>
2023-09-09 11:31:37 +0900

    feature/fab-character-creation

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 import {GiPencil} from 'react-icons/gi'
+import { useNavigate } from 'react-router-dom'
+import { pathCharacterNew } from '../routes/EndPoints'
 
 function FloatingActionButton() {
+	const navigate = useNavigate()
+
 	return (
 		<button
 			className="fixed bottom-28 right-8 w-16 h-16 bg-primary rounded-full shadow-lg hover:bg-primary-light transition duration-300"
 			onClick={() => {
-				// ボタンがクリックされたときの処理を追加
+				navigate(pathCharacterNew())
 			}}
 		>
 			<div className="flex items-center justify-center h-full">
